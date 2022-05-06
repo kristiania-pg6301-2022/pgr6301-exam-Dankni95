9c66da3ffed022f77710c4b32df365732d812ad0
Daniel Lysak <Daniel_kn@live.no>
2022-05-06 05:28:32 +0200

    gracefully exit/close all tests

--- server/__tests__/articleApi.test.js
+++ server/__tests__/articleApi.test.js
@@ -1,107 +1,111 @@
-import request from "supertest"
-import express from "express"
-import {MongoClient} from "mongodb"
-import dotenv from "dotenv"
-import bodyParser from "body-parser"
-import {ArticlesApi} from "../articlesApi.js";
+import request from "supertest";
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import { ArticlesApi } from "../articlesApi.js";
 import WS from "jest-websocket-mock";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
-const mongoClient = new MongoClient(process.env.MONGODB_URL)
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
 const server = new WS("ws://localhost:1234");
 const client = new WebSocket("ws://localhost:1234");
 
-
 describe("article api", () => {
+  afterAll(async () => {
+    await mongoClient.connect().then(async (con) => {
+      await con.db("test_database").collection("articles").deleteMany({});
+    });
+    client.close();
+    server.close();
+  }, 1000);
 
-    afterAll(async () => {
-        await mongoClient.close()
-        client.close()
-        server.close()
-    })
+  beforeEach(async () => {
+    const db = await mongoClient.connect();
+    await db.db("test_database").collection("articles").deleteMany({});
+    await db.close();
+  });
 
-    beforeEach(async () => {
-        const db = await mongoClient.connect()
-        await db.db("test_database").collection("articles").deleteMany({})
-        await db.close()
+  it("adds a new article", async () => {
+    await server.connected;
+
+    let socket = [];
+
+    socket.push(client);
+
+    const title = "My Test article";
+    const content = "my content";
+    const name = "Danny";
+    const category = "Politics";
+    const date = new Date().toLocaleDateString();
+
+    await mongoClient.connect().then(async (con) => {
+      let connection = await con.db("test_database");
+      app.use("/api/articles", ArticlesApi(await connection, socket));
     });
 
-    it("adds a new article", async () => {
-        const title = "My Test article"
-        const content = "my content"
-        const name = "Danny"
-        const category = "Politics"
-        const date = new Date().toLocaleDateString()
+    expect(
+      (
+        await request(app)
+          .post("/api/articles")
+          .send({ title, content, category, name, date })
+          .expect(200)
+      ).body[0].title
+    ).toEqual(title);
+  });
 
-        await mongoClient.connect()
+  it("adds new article, fails because exists", async () => {
+    const title = "My Test article";
+    const content = "my content";
+    const name = "Danny";
+    const category = "Politics";
+    const date = new Date().toLocaleDateString();
 
+    await server.connected;
+    let socket = [];
+    socket.push(client);
 
-        await server.connected;
+    await mongoClient.connect();
+    const database = await mongoClient.db("test_database");
 
-        let socket = []
+    app.use("/api/articles", ArticlesApi(await database, socket));
 
-        socket.push(client)
+    //insert data
+    await request(app)
+      .post("/api/articles")
+      .send({ title, content, category, name, date })
+      .expect(200);
 
-        const db = await mongoClient.db("test_database")
+    //use data
+    await request(app)
+      .post("/api/articles")
+      .send({ title, content, category, name, date })
+      .expect(400);
+  });
 
+  it("adds a new article, fails when title empty", async () => {
+    const title = "";
+    const content = "my content";
+    const name = "Danny";
+    const category = "Politics";
+    const date = new Date().toLocaleDateString();
 
-        app.use("/api/articles", ArticlesApi(await db, socket))
+    await mongoClient.connect();
+    await server.connected;
 
-        expect(
-            (
-                await request(app).post("/api/articles").send({title, content, category, name, date}).expect(200)
-            ).body
-        ).toEqual([{category: [category.toLowerCase()], content, date, name, title}])
-    })
+    let socket = [];
 
-    it("adds new article, fails because exists", async () => {
-        const title = "My Test article"
-        const content = "my content"
-        const name = "Danny"
-        const category = "Politics"
-        const date = new Date().toLocaleDateString()
+    socket.push(client);
+    const db = await mongoClient.db("test_database");
 
-        await server.connected;
-        let socket = []
-        socket.push(client)
-
-
-        await mongoClient.connect()
-        const database = await mongoClient.db("test_database")
-
-        app.use("/api/articles", ArticlesApi(await database, socket))
-
-        //insert data
-        await request(app).post("/api/articles").send({title, content, category, name, date}).expect(200)
-
-
-        //use data
-        await request(app).post("/api/articles").send({title, content, category, name, date}).expect(400)
-
-    })
-
-    it("adds a new article, fails when title empty", async () => {
-        const title = ""
-        const content = "my content"
-        const name = "Danny"
-        const category = "Politics"
-        const date = new Date().toLocaleDateString()
-
-        await mongoClient.connect()
-        await server.connected;
-
-        let socket = []
-
-        socket.push(client)
-        const db = await mongoClient.db("test_database")
-
-        app.use("/api/articles", ArticlesApi(await db, socket))
-        await request(app).post("/api/articles").send({title, content, category, name, date}).expect(400)
-    })
-})
-
-
+    app.use("/api/articles", ArticlesApi(await db, socket));
+    await request(app)
+      .post("/api/articles")
+      .send({ title, content, category, name, date })
+      .expect(400);
+  });
+});
--- server/__tests__/loginApi.test.js
+++ server/__tests__/loginApi.test.js
@@ -1,46 +1,34 @@
-import request from "supertest"
-import express from "express"
-import { MongoClient } from "mongodb"
-import { LoginApi } from "../loginApi.js"
-import dotenv, {config} from "dotenv"
-import bodyParser from "body-parser"
+import request from "supertest";
+import express from "express";
+import { LoginApi } from "../loginApi.js";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cookieParser(process.env.COOKIE_SECRET))
+const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 describe("login api", () => {
   it("tests google config", async () => {
-    app.use("/api/login", LoginApi())
+    app.use("/api/login", LoginApi());
 
-
-    await request(app)
-      .get("/api/login")
-      .expect(200)
+    await request(app).get("/api/login").expect(200);
     expect(
-        (
-            await request(app).get("/api/login").expect(200)
-        ).body.config.google.client_id
-    ).toContain(process.env.GOOGLE_CLIENT_ID)
-  })
+      (await request(app).get("/api/login").expect(200)).body.config.google
+        .client_id
+    ).toContain(process.env.GOOGLE_CLIENT_ID);
+  });
 
   it("tests openid config", async () => {
-    app.use("/api/login", LoginApi())
+    app.use("/api/login", LoginApi());
 
-
-    await request(app)
-        .get("/api/login")
-        .expect(200)
+    await request(app).get("/api/login").expect(200);
     expect(
-        (
-            await request(app).get("/api/login").expect(200)
-        ).body.config.openid.client_id
-    ).toContain(process.env.IDPORTEN_CLIENT_ID)
-  })
-
-})
-
-
+      (await request(app).get("/api/login").expect(200)).body.config.openid
+        .client_id
+    ).toContain(process.env.IDPORTEN_CLIENT_ID);
+  });
+});
